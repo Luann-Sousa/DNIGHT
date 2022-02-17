7c36a9a51e48aae61fec856a0cc063c22446407e
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-17 02:59:12 -0300

    criação/componets: foi criando o primeiro componente para ser usado de modelo  na criação dos demais.

---
+++ src/components/ComponentExample.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export function ComponentExample() {
+  return (
+    <View style={styles.container}>
+      <Text>Component !</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
